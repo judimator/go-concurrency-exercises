3c695bf41569ff01ada916fc4615c77c430d01c0
Dmitry Dzhumagaliev <judimator@gmail.com>
2021-01-08 14:02:44 +0300

    Finished 1-producer-consumer.

--- 1-producer-consumer/main.go
+++ 1-producer-consumer/main.go
@@ -13,19 +13,19 @@ import (
 	"time"
 )
 
-func producer(stream Stream) (tweets []*Tweet) {
+func producer(stream Stream, buff chan Tweet) {
 	for {
 		tweet, err := stream.Next()
 		if err == ErrEOF {
-			return tweets
+			close(buff)
+			return
 		}
-
-		tweets = append(tweets, tweet)
+		buff <- *tweet
 	}
 }
 
-func consumer(tweets []*Tweet) {
-	for _, t := range tweets {
+func consumer(buff chan Tweet) {
+	for t := range buff {
 		if t.IsTalkingAboutGo() {
 			fmt.Println(t.Username, "\ttweets about golang")
 		} else {
@@ -38,11 +38,12 @@ func main() {
 	start := time.Now()
 	stream := GetMockStream()
 
+	buff := make(chan Tweet)
 	// Producer
-	tweets := producer(stream)
+	go producer(stream, buff)
 
 	// Consumer
-	consumer(tweets)
+	consumer(buff)
 
 	fmt.Printf("Process took %s\n", time.Since(start))
 }
